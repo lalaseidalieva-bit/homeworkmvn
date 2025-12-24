bd74dffa0cb3fe0bbd4bc5d9be3ce36c5a1c8835
Lala Seyid <lalaseidalieva@gmail.com>
2025-12-24 10:28:48 +0400

    additional tests

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -34,4 +34,40 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void shouldCalculateForInregisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 2_000_000;
+        boolean registered = false;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldCalculateForInregisteredAndUnderrLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 2_000;
+        boolean registered = false;
+        long expected = 20;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+
 }
